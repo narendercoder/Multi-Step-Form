c7bcb0b60350b7dc4da7c9cded9d3650331cb644
Narender Singh bisht <89627914+narendercoder@users.noreply.github.com>
2024-04-20 20:49:05 +0530

    updated

--- src/contexts/StepperContext.js
+++ src/contexts/StepperContext.js
@@ -1,5 +1,4 @@
 import { createContext, useContext, useState } from "react";
-import { profession } from "../config/data";
 
 const StepperContext = createContext({ userData: "", setUserData: null });
 
